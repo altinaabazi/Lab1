38471c891449aaf6e977cde8047663b98eaf2a21
altinaabazi <aa64958@ubt-uni.net>
2024-05-12 01:36:18 +0200

    Crud MjeteShkollore

---
+++ Lab1_Backend/Controllers/NgjyraMShController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Lab1_Backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab1_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NgjyraMShController : ControllerBase
+    {
+        private readonly LibrariaContext _context;
+
+        public NgjyraMShController(LibrariaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Tipi
+        [HttpGet]
+        public ActionResult<IEnumerable<NgjyraMSh>> GetNgjyra()
+        {
+            return _context.NgjyraMSh.ToList();
+        }
+
+        // GET: api/Tipi/5
+        [HttpGet("{id}")]
+        public ActionResult<NgjyraMSh> GetNgjyra(int id)
+        {
+            var ngjyra = _context.NgjyraMSh.Find(id);
+
+            if (ngjyra == null)
+            {
+                return NotFound();
+            }
+
+            return ngjyra;
+        }
+
+        // POST: api/Tipi
+        [HttpPost]
+        public async Task<ActionResult<NgjyraMSh>> PostNgjyra(NgjyraMSh ngjyra)
+        {
+            _context.NgjyraMSh.Add(ngjyra);
+            await _context.SaveChangesAsync();
+
+
+            return CreatedAtAction(nameof(GetNgjyra), new { id = ngjyra.ID }, ngjyra);
+        }
+        [HttpPut]
+        public IActionResult PutTipi(NgjyraMSh ngjyra)
+        {
+            if (ngjyra == null)
+            {
+                return BadRequest("Invalid object.");
+            }
+
+      
+
+            _context.Entry(ngjyra).State = EntityState.Modified;
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                throw;
+            }
+            return NoContent();
+        }
+
+
+        // DELETE: api/Tipi/5
+        [HttpDelete("{id}")]
+        public IActionResult DeleteNgjyra(int id)
+        {
+            var ngjyra = _context.NgjyraMSh.Find(id);
+            if (ngjyra == null)
+            {
+                return NotFound();
+            }
+
+            _context.NgjyraMSh.Remove(ngjyra);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+    }
+}
---
+++ Lab1_Backend/Migrations/20240510232432_UpdateMjeteShkollore.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lab1Backend.Migrations.Libraria
+{
+    /// <inheritdoc />
+    public partial class UpdateMjeteShkollore : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DimensionetMSh",
+                table: "MjeteShkollore",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "NgjyraMSh",
+                table: "MjeteShkollore",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProdhuesiMSh",
+                table: "MjeteShkollore",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ShtetiMSh",
+                table: "MjeteShkollore",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "DimensionetMSh",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Dimensione = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DimensionetMSh", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "NgjyraMSh",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Ngjyra = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NgjyraMSh", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProdhuesiMSh",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Prodhuesi = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProdhuesiMSh", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ShtetiMSh",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Shteti = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShtetiMSh", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DimensionetMSh");
+
+            migrationBuilder.DropTable(
+                name: "NgjyraMSh");
+
+            migrationBuilder.DropTable(
+                name: "ProdhuesiMSh");
+
+            migrationBuilder.DropTable(
+                name: "ShtetiMSh");
+
+            migrationBuilder.DropColumn(
+                name: "DimensionetMSh",
+                table: "MjeteShkollore");
+
+            migrationBuilder.DropColumn(
+                name: "NgjyraMSh",
+                table: "MjeteShkollore");
+
+            migrationBuilder.DropColumn(
+                name: "ProdhuesiMSh",
+                table: "MjeteShkollore");
+
+            migrationBuilder.DropColumn(
+                name: "ShtetiMSh",
+                table: "MjeteShkollore");
+        }
+    }
+}

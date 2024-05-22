c3bf3c924a4545ad1e91f789d6ae23049fb06733
blendaabazi <ba64884@ubt-uni.net>
2024-05-22 23:26:09 +0200

    Ruaj ndryshimet lokale para merge

---
+++ Lab1_Backend/Models/KlientiContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Lab1_Backend.Models
+{
+    public class KlientiContext : DbContext
+    {
+        public KlientiContext(DbContextOptions<KlientiContext> options) : base(options)
+        {
+
+        }
+        
+        public DbSet<Klienti> Klienti { get; set; }
+        public DbSet<KlientiGjinia> KlientiGjinia { get; set; } // Add DbSet for Gjinia if it's a separate entity
+        public DbSet<KlientiQyteti> KlientiQyteti { get; set; } // Add DbSet for Qyteti if it's a separate entity
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Klienti>(entity =>
+            {
+                entity.HasKey(e => e.ID);
+                entity.ToTable("Klienti");
+
+                // Map the "Gjinia" property to the database column
+                entity.Property(e => e.GjiniaId)
+                    .IsRequired() // Ensure the property is required
+                    .HasMaxLength(50) // Set maximum length if needed
+                    .IsUnicode(false); // Ensure non-unicode if needed
+                entity.Property(e => e.QytetiId)
+                    .IsRequired() // Ensure the property is required
+                    .HasMaxLength(50) // Set maximum length if needed
+                    .IsUnicode(false); // Ensure non-unicode if needed
+            });
+        }
+    }
+}

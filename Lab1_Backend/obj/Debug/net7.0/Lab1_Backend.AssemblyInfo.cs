fd3410dfc784da9c9d1e46106da8ed3073d95ac9
jonamustafa <jonammustafa@gmail.com>
2024-05-30 22:37:07 +0200

    checking for roles

--- Lab1_Backend/obj/Debug/net7.0/Lab1_Backend.AssemblyInfo.cs
+++ Lab1_Backend/obj/Debug/net7.0/Lab1_Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1_Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20e88beb12decc98279d16c8282666a038ca29a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31804ec954f40864739ede2356f94aaf4f19bc67")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1_Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1_Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
